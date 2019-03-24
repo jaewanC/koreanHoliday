7491d6e6326c83c38d83c2aef51881652b5c15f7
DESKTOP-FVQRKI3\최재완 <zx456498@gmail.com>
2019-03-25 00:07:48 +0900

    [#1] 년도와 월을 입력받아 yaml 파일로 떨궈준다.
    * 추가수정
        * 원하는 경로를 추가로 입력하면 원하는 경로에 생성됨
    * TODO
        * 코드 정리
        * 공공데이터포털에서 제공하는 모든 년도 한번에 다 받는 기능추가
        * ReadMe 수정

---
+++ holiday.js
@@ -0,0 +1,55 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xml2json = require('xml-js');
+var json2yaml = require('json2yaml');
+var fs = require('fs');
+
+var solYear, solMonth, savePath;
+
+solYear = process.argv[2];
+solMonth = process.argv[3];
+savePath = process.argv[4];
+
+if (solYear === undefined || solMonth === undefined) {
+    console.log("Input Error")
+    process.exit(0);
+}
+
+var url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?" +
+            "ServiceKey=%2BjqX%2FEmCdEajxBYwIdpsifTy7ZLPX1HQh1m97fdWWJCN%2FirT%2FLEytfcZxFYG4bdH5PFWMLUGp9sQp7L0lSylHw%3D%3D&";
+url += "solYear=" + solYear + "&";
+url += "solMonth=" + solMonth;
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+httpGetAsync(url, function(res) {
+    var dateInfo = {};
+    var jsonData, jsonDates;
+    var i;
+
+    jsonData = JSON.parse(xml2json.xml2json(res));
+
+    dateInfo['totalDate'] = +jsonData.elements[0].elements[1].elements[3].elements[0].text;
+
+    if (+jsonData.elements[0].elements[1].elements[3].elements[0].text !== 0)
+    {
+        jsonDates = jsonData.elements[0].elements[1].elements[0].elements;
+        dateInfo['dates'] = [];
+        for (i = 0; i < jsonDates.length; i++)
+        {
+            dateInfo['dates'].push({
+                dateName: jsonDates[i].elements[1].elements[0].text,
+                date: jsonDates[i].elements[3].elements[0].text,
+            })
+        }
+    }
+
+    fs.writeFileSync(savePath + solYear + '-' + solMonth + '.yml'  , json2yaml.stringify(dateInfo), 'utf8')
+})
